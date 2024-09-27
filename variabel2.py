5187bbef505714466140f686e97595a005abeb35
Ardiansyah <rain7320redmi@gmail.com>
2024-09-27 21:35:53 +0700

    Add files via upload

---
+++ variabel2.py
@@ -0,0 +1,11 @@
+# variabel a merepresentasikan panjang
+a - 5
+b = # variabel b merepresentasikan tinggi
+
+"""
+Dan variabel merepresentasikan luas
+persegi dari hasil perkalian
+variabel a dan variabel variabel variabel
+"""
+
+c = a * 5
